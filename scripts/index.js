4b2e34b0320e04fe348a8026ed6c6e3ba284405c
wre0rl <wre0rl@gmail.com>
2023-06-01 19:04:05 +0700

    Add show book function

--- scripts/index.js
+++ scripts/index.js
@@ -13,8 +13,35 @@ function addBook(title, author, pages, read) {
 
 function getBooks() {
   for (const book of library) {
-    console.log(book.author);
+    showBook(book.title, book.author, book.pages, book.read)
   }
+}
+
+function showBook(title, author, pages, read) {
+  const booksContainer = document.querySelector('.main');
+  const book = document.createElement('div');
+  book.setAttribute('class', 'main__item');
+  booksContainer.appendChild(book);
+  
+  const bookTitle = document.createElement('div');
+  bookTitle.setAttribute('class', 'main__item-title');
+  book.appendChild(bookTitle);
+  bookTitle.innerText = title;
+
+  const bookAuthor = document.createElement('div');
+  bookAuthor.setAttribute('class', 'main__item-author');
+  book.appendChild(bookAuthor);
+  bookAuthor.innerText = author;
+
+  const bookPages = document.createElement('div');
+  bookPages.setAttribute('class', 'main__item-pages');
+  book.appendChild(bookPages);
+  bookPages.innerText = `${pages} pages`;
+
+  const bookRead = document.createElement('div');
+  bookRead.setAttribute('class', 'main__item-read');
+  book.appendChild(bookRead);
+  bookRead.innerText = read;
 }
 
 function deleteBook(index) {
