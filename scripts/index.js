32c84703cab2160da066a410a2702c007372b727
wre0rl <wre0rl@gmail.com>
2023-06-01 19:38:17 +0700

    Add dummy data

--- scripts/index.js
+++ scripts/index.js
@@ -31,12 +31,12 @@ function showBook(title, author, pages, read) {
   const bookAuthor = document.createElement('div');
   bookAuthor.setAttribute('class', 'main__item-author');
   book.appendChild(bookAuthor);
-  bookAuthor.innerText = author;
+  bookAuthor.innerText = `by ${author}`;
 
   const bookPages = document.createElement('div');
   bookPages.setAttribute('class', 'main__item-pages');
   book.appendChild(bookPages);
-  bookPages.innerText = `${pages} pages`;
+  bookPages.innerText = `${pages}`;
 
   const bookRead = document.createElement('div');
   bookRead.setAttribute('class', 'main__item-read');
@@ -50,6 +50,17 @@ function deleteBook(index) {
 
 addBook('The Hobbit', 'J.R.R. Tolkien', 295, false);
 addBook('Six of Crows', 'Leigh Bardugo', 496, true);
+addBook('Atomic Habits', 'James Clear', 288, false);
+addBook('Intelligent Design', 'William Dembski', 312, false);
+addBook('Blink', 'Malcolm Gladwell', 244, false);
+addBook('Factfulness', 'Malcolm Gladwell', 244, false);
+addBook('Eat That Frog', 'Brian Tracy', 144, false);
+addBook('The Alchemist', 'Paulo Coelho', 208, false);
+addBook('The Last Wish', 'Andrzej Sapkowski', 288, false);
+addBook('1984', 'George Orwell', 328, false);
+addBook('On the Road', 'Jack Kerouac', 307, false);
+addBook('Baptism of Fire', 'Andrzej Sapkowski', 343, false);
+addBook('Time of Contempt', 'Andrzej Sapkowski', 331, false);
 getBooks();
 //deleteBook(0);
 //getBooks();
